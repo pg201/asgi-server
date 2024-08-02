074503540551d815ae8df28893e7b4993e4f821d
pgtheman1 <pgpg79860@gmail.com>
2024-08-02 02:11:25 -0500

    updated file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="asgi",
-    description="A tutorial implementation of an ASGI server and application.",
+    description="A implementation of an ASGI server and application.",
     version="0.0.1",
     packages=find_packages(),
 )
